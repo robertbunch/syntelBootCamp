f3dc60513fb37e593a59feab37fc7b96516cec81
Robert Bunch <rob@digitalcrafts.com>
2018-05-08 15:35:45 -0400

    Finish adding html to the DOM

--- js/weatherWidget.js
+++ js/weatherWidget.js
@@ -10,7 +10,24 @@ $(document).ready(()=>{
 		const userCity = $('#city').val()
 		// const userCity = document.getElmenetById('city').value
 		// console.log(userCity);
-		const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${userCity}&units=imperial&appid=${apiKey}`
-		console.log(weatherUrl);
+		const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${userCity}&units=metric&appid=${apiKey}`
+		// console.log(weatherUrl);
+		// getJSON:
+		// 1. WHERE (url)
+		// 2. Callback to run when done
+		$.getJSON(weatherUrl,(weatherData)=>{
+			console.log(weatherData);
+			const currTemp = weatherData.main.temp;
+			const icon = weatherData.weather[0].icon;
+			const high = weatherData.main.temp_max;
+			const weatherHTML = `
+				<div>
+					<img src="http://openweathermap.org/img/w/${icon}.png" />
+					<div class="col-sm-3">The temp in ${weatherData.name} is ${currTemp}</div>
+					<div class="col-sm-3">The high is forecasted at ${high}</div>
+				</div>
+			`
+			$('#weather-info').html(weatherHTML);
+		})
 	});
 })
