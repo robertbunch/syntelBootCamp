82b19296b7772e917aae67a5a40d058f341ac7cb
Robert Bunch <rdbunch@gmail.com>
2018-05-14 15:43:26 -0400

    Add additem

---
+++ node/to_do_sql/routes/index.js
@@ -0,0 +1,64 @@
+var express = require('express');
+var router = express.Router();
+// Import mysql. We got this from npm.
+const mysql = require('mysql');
+const db_creds = require('../config/config.js');
+// Set up a connection to use and reuse...
+const connection = mysql.createConnection(db_creds)
+
+// We made a connection above, now actually run it
+connection.connect();
+// AFTER ALL THAT PAIN... WE CAN START WRITING AWESOME QUERIES.
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+	// get the msg var out of the query string
+	let message = req.query.msg;
+	if(message === 'added'){
+		message = `Your task was added!`;
+	}
+
+  res.render('index', { 
+  	message,
+  	title: 'Express',
+  	taskArray: []
+  });
+});
+
+router.post('/addItem',(req, res)=>{
+	const taskName = req.body.newTask;
+	const taskDate = req.body.newTaskDate
+	// res...
+	// 1. send
+	// 2. render
+	// 3. json
+	// req.query is for get requests or query string
+	// req.body is for data passed in via post
+	// we know that someone submitted the form. We know that
+	// because they are at the post, addItem route.
+	// we got the data the form sent out of the req.body
+	// object and stored it. Now... we need to put it into
+	// our DB (mysql)
+
+	const insertQuery = `
+	INSERT into tasks
+		(task_name, task_date)
+		VALUES
+		(?,?)`;
+	// query args:
+	// 1. QUery string
+	// 2. array corresponding to ? in query
+	// 3. callback to run when query is finished
+
+	console.log(`$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ READY TO RUN THE QUERY $$$$$$$$$$$$$$$$$$$$$$`);
+
+	connection.query(insertQuery,[taskName,taskDate],(error, results)=>{
+		if(error) throw error;
+		console.log(`=================QUERY DID NOT ERROR< SENDING TO THE HOMEPAGE===================`)
+		res.redirect('/?msg=added')
+	})
+
+	// res.json(req.query);
+})
+
+module.exports = router;
