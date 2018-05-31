db70bec904106e6f9812bd896b6b69d40ff9175a
Robert Bunch <rob@digitalcrafts.com>
2018-05-31 12:08:39 -0400

    Add connect module

--- react/redux101/src/index.js
+++ react/redux101/src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// We need a way to get the store to react. The module for the job is react-redux. react-redux is the glue in between react and redux. It's like the controller in between the model and the view.
+// React-redux gives us a component called Provider. The Provider component is around EVERYTHING that needs the glue.
+import { Provider } from 'react-redux';
+
+
 // Import the store from redux so React components can use it
 import { createStore } from 'redux';
 
@@ -16,9 +21,6 @@ const theStore = createStore(reducers);
 // HOORAY! WE HAVE A STORE WITH STUFF IN IT. REDUX, OUT.
 // -- LBJ
 
-// We need a way to get the store to react. The module for the job is react-redux. react-redux is the glue in between react and redux. It's like the controller in between the model and the view.
-// React-redux gives us a component called Provider. The Provider component is around EVERYTHING that needs the glue.
-import { Provider } from 'react-redux';
 
 // We will still render the App component. BUT, we will render it inside of the Provider component.
 // Provider component takes a prop of store which will be the Redux store we made above.
