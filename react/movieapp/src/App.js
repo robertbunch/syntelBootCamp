8faadb6075c7ba400b599c38ee6adbea73359cb0
Robert Bunch <rob@digitalcrafts.com>
2018-05-31 11:39:18 -0400

    Add redux boilerplate

--- react/movieapp/src/App.js
+++ react/movieapp/src/App.js
@@ -34,60 +34,36 @@ class App extends Component {
   // will run as soon as the FIRST render runs
   // this is where AJAX goes
   componentDidMount(){
-    console.log("THe component has mounted!");
-    const url = 'http://api.themoviedb.org/3/movie/now_playing?api_key=fec8b5ab27b292a68294261bb21b04a5'
-    // axios.get(url)
-    $.getJSON(url,(movieData)=>{
-      console.log(movieData);
-      this.setState({
-        moviePosters: movieData.results
-      });
-    });
+    // NOWPLAYING REQUEST HAS BEEN MOVED TO 
+    // THE MOVIESEARCH COMPONENT
   };
 
   newUserSearch(movieTitle){
-    const url = `https://api.themoviedb.org/3/search/movie?api_key=fec8b5ab27b292a68294261bb21b04a5&query=${movieTitle}`;
-    $.getJSON(url,(movieData)=>{
-      console.log(movieData);
-      this.setState({
-        moviePosters: movieData.results
-      })
-    })
+    // REMOVED ALL THIS
+    // IT HAS BEEN ABSTRACTED TO SEARCHBAR
   }
 
 // EVERY React component MUST have a render
   render() {
 
-    if(this.state.moviePosters.length === 0){
-      return (<h1>Loading...</h1>)
-    }
+    // if(this.state.moviePosters.length === 0){
+    //   return (<h1>Loading...</h1>)
+    // }
 
-    console.log(this.state.moviePosters)
-    const posters = this.state.moviePosters.map((aMovie,index)=>{
-      const imagePath =  `http://image.tmdb.org/t/p/w300${aMovie.poster_path}`;
-      return (<Poster name="Snoopy" movie={aMovie} poster={imagePath} />)
-    });
-    console.log(posters);
-    console.log("Render is running...")
-    // return a single dom element (jsx_)
+    // console.log("Render is running...")
+    // return a single dom element (jsx)
     return (
       // ROUTER GOES AROUND EVERYTHING
       <Router>
         <div className="App">
           <Route path="/" component={BootstrapNavBar} />
           <Route path="/" component={SearchBar} />
+          <Route exact path="/" component={MovieSearch} />
           <Route exact path="/about" component={About} />
           <Route exact path = "/login" component={Login} />
           <Route exact path = "/register" component={Register} />
 {/*          You dont always have to give it a component.
           You can give it a callback which can render componenets*/}
-          <Route exact path="/" render={()=>{
-            return(
-              <div>
-                {posters}
-              </div>
-            )
-          }} />
           <Route exact path="/search/:searchTerm" component={MovieSearch} />
           <Route exact path="/movie/:movieId" component={SingleMovie} />
         </div>
---
+++ react/redux101/src/reducers/HomeReducer.js
@@ -0,0 +1,13 @@
+// A reducer is a FUNCTION.
+// That function returns a peice of state.
+// A piece of state is just an object.
+
+// ALL reducer functions have 2 params in their signiture
+// 1. Current value of it's state (obj)
+// 1b. Thanks to ES6, you will usually want to provide
+// a default value for state, in case it's not set
+// 2. info that came from the dispatcher
+
+export default function(state = "Home Page Text", action){
+	return state;
+}
